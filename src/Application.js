1d20ce9cb25ad58f1f03f1571d1e04438aaa2595
Steve Kinney <hello@stevekinney.net>
2019-11-21 16:14:45 -0600

    Add undo

--- src/Application.js
+++ src/Application.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Grudges from './Grudges';
 import NewGrudge from './NewGrudge';
+import { GrudgeContext } from './GrudgeContext';
 
 const Application = () => {
+  const { undo, isPast } = useContext(GrudgeContext);
+
+  console.log({ undo, isPast });
+
   return (
     <div className="Application">
       <NewGrudge />
+      <section>
+        <button disabled={!isPast} onClick={undo}>
+          Undo
+        </button>
+        <button>Redo</button>
+      </section>
       <Grudges />
     </div>
   );
